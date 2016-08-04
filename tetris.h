eddc2cfbda4614b33525830b9637673a3ed118ac
Ernesto Alfonso <erjoalgo@gmail.com>
2016-08-04 11:04:16 -0700

    add global default width, height

--- tetris.h
+++ tetris.h
@@ -66,3 +66,6 @@ void block_rotate(block* b, int amount);
 void block_move ( block* b, direction d, int amount );
 void block_crust_get ( block* b, direction d, int i, coord* result );
 int block_extreme(block* b, direction d);
+
+#define GRID_HEIGHT 19
+#define GRID_WIDTH 10
