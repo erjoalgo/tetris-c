362fc208d58e6edcf986d9bf287810d1eb673556
Ernesto Alfonso <erjoalgo@gmail.com>
2017-11-26 20:33:21 -0800

    factor out game flow from {human,ai}_get_move

--- tetris-play.c
+++ tetris-play.c
@@ -9,9 +9,8 @@
 
 #define DELAY 300000
 
-
-void human_play();
-void ai_play(int depth, int delay_secs);
+void play();// human or ai
+void ai_play(int depth, int delay_secs);// ai only
 
 int main(int argc, char** argv)
 {
@@ -25,7 +24,7 @@ int main(int argc, char** argv)
   }else 	{
     char* opt = argv[1];
     if (!strcmp(argv[1], "human"))	{
-      human_play();
+      play();
     }else if (!strcmp(opt, "ai"))	{
       ai_play(3, 1);
     }else 	{
@@ -36,32 +35,14 @@ int main(int argc, char** argv)
   return 0;
 }
 
-void human_play() {
+int human_get_move ( grid* g, block* b, shape_stream* ss )	{
+  // provide the human's move as a mutation of b
+  // return whether there was a drop request (i.e. ready for next block)
+  // don't change the grid
 
-  grid* g = grid_new(GRID_HEIGHT, GRID_WIDTH);
-  block* b = block_new(NULL);
-
-  ncurses_setup(g);
-  ncurses_refresh();
-
-  int dropped = 1, cleared = 0;
-  int color;
-
-  while(1) {
-    if (dropped)	{
-      color = RAND(SHAPE_COUNT);
-      block_init(b, SHAPES[color]);
-      grid_block_center_elevate(g, b);
-      if (grid_block_intersects(g, b))	{
-	mvprintw(g->height+1, 0, "game over!");
-	break;
-      }
-      ncurses_block_print_shadow(b, 1, g);
-      ncurses_refresh();
-      dropped = 0;
-    }else 	{
+  (void)ss;//could be used to display the future blocks
+  int dropped = 0;
       int ch = getch();
-      ncurses_block_print_shadow(b, 0, g);//delete block
       switch(ch){
       case KEY_LEFT: grid_block_move_safe(g, b, LEFT, 1); break;
       case KEY_RIGHT: grid_block_move_safe(g, b, RIGHT, 1); break;
@@ -79,26 +60,86 @@ void human_play() {
 	}
       }
       }
+  return dropped;
+}
+
+game_move* gm;
+int ai_get_move ( grid* g, block* b, shape_stream* ss)	{
+  int drop = 0;
+  if (gm == NULL)	{
+    // new block. just display it
+    gm = ai_best_move(g, ss, default_weights);
+  }else 	{
+    // make moves one at a time. rotations first
+    if (b->rot != gm->rot)	{
+      int inc = (gm->rot-b->rot+4)%4;
+      b->rot = (b->rot+4+(inc<3? 1: -1))%4;
+    }else if (b->offset[0] != gm->col)	{
+      b->offset[0]+=gm->col>b->offset[0]?1: -1;
+    }else 	{
+      drop = 1;
+      gm = NULL;
+    }
+  }
+  sleep_secs(1);
+  return drop;
+}
+
+int ai_playing = 1;
+
+void play() {
+
+  grid* g = grid_new(GRID_HEIGHT, GRID_WIDTH);
+  block* b = block_new(NULL);
+  block* b_old = block_new(NULL);
+
+  ncurses_setup(g);
+  ncurses_refresh();
+
+  int dropped = 1;
+
+  // allow ai to make moves for human
+  ai_init();
+  shape_stream* ss = shape_stream_new(1);
+
+  while(1) {
+    if (dropped)	{
+      // generate a new block
+      shape_stream_pop(ss);
+      block_init(b, shape_stream_peek(ss, 0));
+      grid_block_center_elevate(g, b);
+      if (grid_block_intersects(g, b))	{
+	break;// cannot place new block. game over
+      }
+      ncurses_block_print_shadow(b, 1, g);
+      ncurses_refresh();
+      if (ai_playing)	{
+	sleep_secs(1);//brief pause to simulate 'ai thinking'
+      }
+
+      dropped = 0;
+    }else	{
+      block_cpy(b_old, b);// remember where previous block was, to erase it
+      dropped = ai_playing? ai_get_move(g, b, ss) : human_get_move(g, b, ss);
+      ncurses_block_print_shadow(b_old, 0, g);//unpaint old block
+      int cleared = 0;
       if (dropped)	{
 	grid_block_drop(g, b);
 	grid_block_add(g, b);
 	cleared = grid_clear_lines(g);
+      }
 	if (cleared)	{
 	  // need to repaint the whole grid
 	  ncurses_grid_print(g);
-	}
 	ncurses_grid_print_fill_count(g);
       }else 	{
-	cleared = 0;
-      }
-      if (!cleared)	{
 	ncurses_block_print_shadow(b, 1, g);//repaint in new location
       }
-
-      ncurses_refresh();
     }
+    ncurses_refresh();
   }
 
+  mvprintw(g->height+1, 0, "game over!");
   endwin();
 }
 
