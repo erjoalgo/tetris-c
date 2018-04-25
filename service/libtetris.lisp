20901dabc6d827cb82eda700078029e2e4b8a0b3
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-24 23:34:31 -0700

    factor out poc code into smaller functions
    starts to define the libtetris interface

--- service/libtetris.lisp
+++ service/libtetris.lisp
@@ -34,74 +34,103 @@
 
 (init-tetris seed shapes-file)
 
-(format t "starting test...~%" )
+(defun grid-test ()
+  (format t "starting test...~%" )
+  (cffi:foreign-funcall "grid_test"))
 
-(cffi:foreign-funcall "grid_test")
+(defvar dbg)
+
+(defstruct game
+  g ;; grid
+  b ;; block
+  ss ;; shape-stream
+  height
+  width
+  )
+
+(defun game-init (height width)
+  (cffi:with-foreign-objects
+      ((g :pointer)
+       (b :pointer)
+       (ss :pointer)
+       )
+
+    (format t "initializing grid...~%" )
+    (setf g (cffi:foreign-funcall "grid_new" :int height :int width :pointer))
+
+    (format t "initializing block...~%" )
+    (setf b (cffi:foreign-funcall "block_new" :pointer))
+
+    (format t "initializing ss...~%" )
+    (setf ss (cffi:foreign-funcall "shape_stream_new" :int 3 :pointer))
+    (make-game :g g :b b :ss ss :height height :width width)
+    ))
+
+(defun game-next-shape (game)
+  (let* ((ss (game-ss game))
+         (next-shape (cffi:foreign-funcall "shape_stream_pop"
+                                           :pointer ss
+                                           :pointer)))
+
+
+    (format t "initializing block shape...~%" )
+
+    (setf next-shape (cffi:foreign-funcall "block_init"
+                                           :pointer (game-b game)
+                                           :pointer next-shape
+                                           :void))
+
+    (cffi:foreign-funcall "grid_block_center_elevate"
+                          :pointer (game-g game)
+                          :pointer (game-b game))
+
+    ;; return whether new block could be successfully placed
+    (if (not (cffi:foreign-funcall "grid_block_intersects"
+                                   :pointer (game-g game)
+                                   :pointer (game-b game)
+                                   :boolean))
+        next-shape
+        nil;; game over
+        )))
+
+
+
+(defun game-exec-move (game move)
+  (declare (ignore move))
+  ;; TODO set block to move
+  (cffi:foreign-funcall "grid_block_drop"
+                        :pointer (game-g game)
+                        :pointer (game-b game)
+                        :int)
+
+  (cffi:foreign-funcall "grid_block_add"
+                        :pointer (game-g game)
+                        :pointer (game-b game)
+                        :int))
+
+(defun game-print (game)
+  (format t "~{~a~%~}"
+          (loop
+             with rows = (cffi:mem-ref (game-g game) :pointer 0)
+             for r downfrom (1- (game-height game)) to 0
+             as row = (cffi:mem-aref rows :pointer r)
+             collect
+               (format nil "~{~d~}"
+                       (loop for c below (game-width game)
+                          as val = (cffi:mem-aref row :int c)
+                          do (format t "val ~D~%" val)
+                          do (setf dbg val)
+                          collect val)))))
 
 (defparameter HEIGHT 19)
 (defparameter WIDTH 10)
 
-(defvar dbg)
+(defun test-game ()
+  (let ((game (game-init HEIGHT WIDTH))
+        next-shape)
+    (setf next-shape (game-next-shape game))
+    (game-exec-move game nil)
+    (game-print game)))
 
-(cffi:with-foreign-objects
-    ((g :pointer)
-     (b :pointer)
-     (ss :pointer)
-     (next-shape :pointer)
-     )
 
-  (format t "initializing grid...~%" )
-  (setf g (cffi:foreign-funcall "grid_new" :int HEIGHT :int WIDTH :pointer))
-
-  (format t "initializing block...~%" )
-  (setf b (cffi:foreign-funcall "block_new" :pointer))
-  (format t "initializing ss...~%" )
-  (setf ss (cffi:foreign-funcall "shape_stream_new" :int 3 :pointer))
-
-  (format t "initializing next shape... ~A~%" ss)
-
-  (setf next-shape (cffi:foreign-funcall "shape_stream_pop"
-                                              :pointer ss
-                                              :pointer))
-
-  (format t "initializing block shape...~%" )
-
-  (cffi:foreign-funcall "block_init"
-                        :pointer b
-                        :pointer next-shape
-                        :void)
-
-  (cffi:foreign-funcall "grid_block_center_elevate"
-                        :pointer g
-                        :pointer b)
-
-  (if (cffi:foreign-funcall "grid_block_intersects"
-                            :pointer g
-                            :pointer b
-                            :boolean)
-      (format t "cannot plane new block~%" )
-      (format t "can continue"))
-
-  (cffi:foreign-funcall "grid_block_drop"
-                        :pointer g
-                        :pointer b
-                        :int)
-
-  (cffi:foreign-funcall "grid_block_add"
-                        :pointer g
-                        :pointer b
-                        :int)
-  (format t "~{~a~%~}"
-          (loop
-             with rows = (cffi:mem-ref g :pointer 0)
-             for r downfrom (1- HEIGHT) to 0
-             as row = (cffi:mem-aref rows :pointer r)
-             collect
-               (format nil "~{~d~}"
-                       (loop for c below WIDTH
-                          as val = (cffi:mem-aref row :int c)
-                          do (format t "val ~D~%" val)
-                          do (setf dbg val)
-                          collect val))))
-
-  )
+(test-game)
