186962280a3d555449ce4528a8add6862e3ed680
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-26 19:27:36 -0700

    implement server-side to original client
    except provide separate, named REST endpoints
    for different request codes

--- service/server.lisp
+++ service/server.lisp
@@ -15,15 +15,41 @@
 
 (defvar dbg)
 
-(hunchentoot:define-easy-handler (tetris :uri "/tetris.uwsgi") ()
-  (setf dbg (list hunchentoot:*request* hunchentoot:*acceptor*))
-  ;; (format t "holahola~%" )
-  (let* ((qstring (hunchentoot:query-string*)))
-    (cond
-      ((string= "-1" qstring) "init request")
-      (t (or (ppcre::register-groups-bind (move-no game-no) ("([0-9]+)%([0-9]+)" qstring)
-               (format nil "game ~A, move ~A~%" game-no move-no))
-             "invalid request")))))
+(defparameter *curr-gameno* 3)
+
+(hunchentoot:define-easy-handler (game-status :uri "/game-status") ()
+  (let* ((game-no *curr-gameno*)
+         (move-no 0)
+         (game (car (gethash game-no games)))
+         resp)
+
+    (if (null game)
+        ;; TODO non-200
+        "no current games"
+        (progn
+          (setf resp (list (libtetris:game-height game) (libtetris:game-width game) move-no game-no))
+          '(libtetris:game-grid-iter game (lambda (r c v) (when (not (zerop v))
+                                                            (push c resp)
+                                                            (push r resp))))
+          (format nil "[~{~D~^, ~}]" resp)))))
+
+(hunchentoot:define-easy-handler (move :uri "/moves") (game-no move-no)
+  ;; (setf (hunchentoot:content-type*) "text/plain")
+  (let ((moves (cdr (gethash game-no games))))
+    (if (null moves) "no such game" ;; TODO non-200
+        (loop while (>= move-no (length moves))
+           do (progn (format t "waiting for game to catch up to from ~D to ~D on game ~D~%"
+                             (length moves) move-no game-no)
+                     (sleep 1))
+           finally
+             (return (format nil "[~{~D~^, ~}]"
+                             ;; TODO export
+                             (with-slots (libtetris::shape-code libtetris::rot libtetris::col)
+                                 (aref moves move-no)
+                               (list libtetris::shape-code libtetris::rot
+                                     libtetris::col))))))))
+
+(defvar games (make-hash-table))
 
 (defun test-curl ()
   (with-output-to-string
@@ -36,5 +62,36 @@
                         :wait t
                         :output output-fh
                         :error output-fh)))
+
+(defun game-run (game moves)
+  (loop
+     for i from 0
+     upto 100
+     as next-move = (libtetris:game-apply-next-move game)
+     while next-move
+     do (libtetris:game-print game)
+     do
+       (let ((native (libtetris:my-translate-from-foreign next-move)))
+         (progn
+           (format t "on move ~D, shape ~D, rot ~D, col ~D~%" i
+                   (slot-value native 'libtetris::shape-code)
+                   (slot-value native 'libtetris::rot)
+                   (slot-value native 'libtetris::col))
+           (sleep .5)
+           (vector-push-extend native moves)))))
+
+(defun game-create (game-no)
+  (let ((moves (make-array 0 :adjustable t
+                           :fill-pointer t
+                           :element-type 'libtetris:game-move-native))
+        (game (libtetris:game-init libtetris:HEIGHT libtetris:WIDTH libtetris:ai-default-weights)))
+    (setf (gethash game-no games) (cons game moves))))
+
+(defun game-create-run (game-no)
+  (destructuring-bind (game . moves) (game-create game-no)
+      (game-run game moves)))
+
+(defun game-create-run-thread (game-no)
+  (sb-thread:make-thread 'game-create-run :arguments (list game-no)))
 
 (format t "test curl: ~A~%" (test-curl))
