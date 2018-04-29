511ca518e3dba9981a8c42209a63c5237e79b00b
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-29 13:10:08 -0700

    capture sexp to load from REPL

---
+++ service/repl-load.lisp
@@ -0,0 +1,8 @@
+(progn (ql:quickload "hunchentoot") (ql:quickload "cffi")
+       (ql:quickload "drakma") (ql:quickload "stefil")
+       (ql:quickload "jonathan")
+       (load "libtetris.lisp")
+       (load "server.lisp")
+       (load "test/server-test.lisp")
+       (swank:set-package "SERVER-test")
+       )
