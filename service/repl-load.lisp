50eb8c278cc11cd683908d06ad038bdb5c316452
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-30 14:59:09 -0700

    add sexps to start service and one game

--- service/repl-load.lisp
+++ service/repl-load.lisp
@@ -7,3 +7,7 @@
        (load "test/server-test.lisp")
        (swank:set-package "SERVER-test")
        )
+
+(progn
+  (service-start nil :port 4242)
+  (game-create-run-thread 0 :ai-move-delay-secs .5))
