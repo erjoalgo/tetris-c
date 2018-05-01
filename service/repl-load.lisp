3824ae00c718c37bc8b8ace1a8ba66168c0f9d4f
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-30 18:49:25 -0700

    add kill-tetris-threads to debug 503 err message

--- service/repl-load.lisp
+++ service/repl-load.lisp
@@ -8,6 +8,18 @@
        (swank:set-package "SERVER-test")
        )
 
+
+(defun s-starts-with-p (prefix string)
+  (and (<= (length prefix) (length string))
+       (string= prefix (subseq string 0 (length prefix)))))
+
+;; (defun kill-tetris-threads ()
+;;   (loop for thread in (sb-thread:list-all-threads)
+;;      as name = (sb-thread:thread-name thread)
+;;      if (s-starts-with-p server::thread-name-prefix name)
+;;      do (progn (format t "killing ~A~%" name)
+;;                (sb-thread:terminate-thread thread))))
+
 (progn
   (service-start nil :port 4242)
   (game-create-run-thread 0 :ai-move-delay-secs .5))
