2f8a0662af1c13a1b1a8315bbaca0f659b90f981
Ernesto Alfonso <erjoalgo@gmail.com>
2018-04-30 18:50:40 -0700

    add misc debugging sexps

--- service/repl-load.lisp
+++ service/repl-load.lisp
@@ -8,6 +8,9 @@
        (swank:set-package "SERVER-test")
        )
 
-(progn
-  (service-start nil :port 4242)
+'(progn
+  (setf hunchentoot:*catch-errors-p* nil)
+  (vom:config t :debug4)
+  (service-stop)
+  (service-start nil :port 4242 :MAX-MOVE-CATCHUP-WAIT-SECS 1)
   (game-create-run-thread 0 :ai-move-delay-secs .5))
