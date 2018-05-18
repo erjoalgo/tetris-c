c4abfc49385d1588777f0b6bda6d118ad9f0608c
Ernesto Alfonso <erjoalgo@gmail.com>
2018-05-18 12:16:59 -0700

    add excessive verbose logging of clws

--- service/main.lisp
+++ service/main.lisp
@@ -28,6 +28,7 @@
     (t
      (when verbose
        (vom:config t :debug)
+       (setf clws:*log-level* t)
        (vom:debug "verbose enabled"))
      (let ((config (apply 'make-config (append args '(:allow-other-keys t)))))
        (when dims
