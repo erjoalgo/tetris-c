25931d9a06b5294e90f8f25371f59016da01ece7
Ernesto Alfonso <erjoalgo@gmail.com>
2018-05-18 10:47:01 -0700

    set default verbose to 'debug', excessive log to debug4

--- service/main.lisp
+++ service/main.lisp
@@ -25,7 +25,7 @@
     (help (command-line-arguments:show-option-help +service-option-spec+ :sort-names t))
     (version (format t "~A~%" *version*))
     (t
-     (when verbose (vom:config t :debug4))
+     (when verbose (vom:config t :debug))
      (let ((config (apply 'make-config (append args '(:allow-other-keys t)))))
        (when dims
          (ppcre:register-groups-bind ((#'parse-integer h) (#'parse-integer w))
