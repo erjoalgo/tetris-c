36f623df76a2bda2fd540ede266cc90036397b71
Ernesto Alfonso <erjoalgo@gmail.com>
2018-06-20 21:38:54 -0700

    fix conflicting short flag character

--- service/main.lisp
+++ service/main.lisp
@@ -6,7 +6,7 @@
   '(
     (("port" #\p) :type integer :optional t :documentation "service port")
     (("shapes-file" #\s) :type string :optional t :documentation "path to a libtetris shapes.in")
-    (("ai-weights-file" #\s) :type string :optional t :documentation "path to a libtetris ai weights file")
+    (("ai-weights-file" #\a) :type string :optional t :documentation "path to a libtetris ai weights file")
     (("seed" #\e) :type integer :optional t :documentation "libtetris ai seed to use")
     (("dims" #\g) :type string :optional t :documentation
      "dimensions of the grid, in the form of HxW, e.g. 19x10")
